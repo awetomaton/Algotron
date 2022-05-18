b08c45c60610bdc41780a8e5645ac5b692b6a691
Jacob Duckett <jduckett@awetomaton.com>
2022-05-18 09:52:36 -0400

    Added MinIO example material

--- setup/rancher-desktop-bootstrap.sh
+++ setup/rancher-desktop-bootstrap.sh
@@ -6,3 +6,6 @@ kubectl create namespace workflows
 helm install argo-workflows argo/argo-workflows -n workflows --set server.extraArgs={--auth-mode=server},server.serviceType="NodePort",server.serviceNodePort=30010
 helm install argo-events argo/argo-events -n workflows
 helm install argo-artifacts bitnami/minio -n workflows --set fullnameOverride=argo-artifacts --set auth.rootUser=admin,auth.rootPassword=adminpass --set service.type=NodePort,service.nodePorts.api=30011,service.nodePorts.console=30012
+
+# https://github.com/argoproj/argo-workflows/blob/master/docs/service-accounts.md - grant admin privileges
+# Manually patch workflow server with info in rancher-desktop-artifact-repository-configmap.yaml
